705bcfc74fece2cc05ef5726d612e4590c7ee730
ampersand-paris <ampersand.s.paris@gmail.com>
2023-08-15 16:02:03 -0400

    colors

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,16 +8,22 @@ import useFetch from "../useFetch";
 
 const About = () => {
 
-    const { isLoading, error, data } = useFetch(`${process.env.REACT_APP_BACKEND}api/about`)
+    const { isLoading, error, data } = useFetch(`${process.env.REACT_APP_BACKEND}api/about?populate=*`)
+    const [backgroud, setBackground] = useState(' ');
 
-    let about = []
-    
+    let about = [];
+    let image = [];
+
     if (data) {
-        about = data.data
-
+        about = data.data.attributes
+        image = data.data.attributes.Background_Image.data[0].attributes.url
+        console.log(image)
+        // style={{backgroundImage: about.Background_Image}}
         return (
-            <div className="">
-               <h1>Hello World!</h1>
+            <div className="about-wrapper" style={{backgroundImage: `url(http://localhost:1337${image})`}}>
+               <h1>{about.Name}</h1>
+               <p>{about.Bio}</p>
+               <p>{about.Artist_Statement}</p>
             </div>                  
         )
     }
